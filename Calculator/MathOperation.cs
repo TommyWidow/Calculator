0bcd5266221fc3d272eb575a500cd11065da5e57
VLAD <VLAD@DESKTOP-G3I8QI5>
2025-02-03 20:46:44 +0300

    Добавьте файлы проекта.

---
+++ Calculator/MathOperation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MathTools
+{
+    class MathOperation
+    {
+        public static double Addition(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Minus(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multi(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Devide(double a, double b)
+        {
+            return a / b;
+        }
+
+        public static double Max(double a, double b)
+        {
+            return a > b ? a : b;
+        }
+
+        public static double Min(double a, double b)
+        {
+            return a < b ? a : b;
+        }
+
+        public static double Abs(double a)
+        {
+            return a < 0 ? -a : a;
+        }
+    }
+}
+
+
+
---
+++ Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using MathTools;
+
+class Calculator
+{
+    static void Main() 
+    {
+        Console.WriteLine("Введите два числа через пробел: ");
+        string input = Console.ReadLine();
+        string[] parts = input.Split(' ');
+
+        double a = double.Parse(parts[0], CultureInfo.InvariantCulture);
+        double b = double.Parse(parts[1], CultureInfo.InvariantCulture);
+
+        Console.WriteLine($"\nResault of Add: {MathOperation.Addition(a, b)}");
+        Console.WriteLine($"Resault of Min: {MathOperation.Minus(a, b)} ");
+        Console.WriteLine($"Resault of Mult: {MathOperation.Multi(a, b)} ");
+        Console.WriteLine($"Result of Div: {MathOperation.Devide(a, b)}");
+        Console.WriteLine($"Result of Max: {MathOperation.Max(a, b)}");
+        Console.WriteLine($"Minimum number of a and b: {MathOperation.Min(a, b)}");
+        Console.WriteLine($"Abs a: {MathOperation.Abs(a)}");
+        Console.WriteLine("\n\n\n\tThis app was created by TommyWindows.");
+    }
+}
