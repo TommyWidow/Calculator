c7334b398ecd0f3a3737ffa559b2db5b3da474b1
TommyWidow <lalatina223@icloud.com>
2025-02-04 19:06:15 +0300

    ver 1.1

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,27 +1,64 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using input;
 using MathTools;
 
 class Calculator
 {
-    static void Main() 
+    static void Main()
     {
-        Console.WriteLine("Введите два числа через пробел: ");
-        string input = Console.ReadLine();
-        string[] parts = input.Split(' ');
+        Input input = new Input();
+        string string1 = "Enter two numbers separated by a space: ";
 
-        double a = double.Parse(parts[0], CultureInfo.InvariantCulture);
-        double b = double.Parse(parts[1], CultureInfo.InvariantCulture);
+        input.InputString(string1);
+        Console.WriteLine();
 
-        Console.WriteLine($"\nResault of Add: {MathOperation.Addition(a, b)}");
-        Console.WriteLine($"Resault of Min: {MathOperation.Minus(a, b)} ");
-        Console.WriteLine($"Resault of Mult: {MathOperation.Multi(a, b)} ");
-        Console.WriteLine($"Result of Div: {MathOperation.Devide(a, b)}");
-        Console.WriteLine($"Result of Max: {MathOperation.Max(a, b)}");
-        Console.WriteLine($"Minimum number of a and b: {MathOperation.Min(a, b)}");
-        Console.WriteLine($"Abs a: {MathOperation.Abs(a)}");
-        Console.WriteLine(" ");
-        Console.WriteLine(" ");
-        Console.WriteLine(" ");
-        Console.WriteLine("\n\n\n\tThis app was created by TommyWindows.");
+        double a = 0, b = 0;
+        bool validInput = false;
+
+        while (!validInput)
+        {
+            try
+            {
+                string inputs = Console.ReadLine();
+                string[] parts = inputs.Split(' ');
+
+                if (parts.Length != 2)
+                {
+                    throw new FormatException("You need to enter two numbers.");
+                }
+
+                a = double.Parse(parts[0], CultureInfo.InvariantCulture);
+                b = double.Parse(parts[1], CultureInfo.InvariantCulture);
+
+                validInput = true;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Input error\n");
+                Console.WriteLine("Try again.");
+            }
+        }
+
+        var (absA, absB) = MathOperation.Abs(a, b);
+
+        List<string> results = new List<string>
+        {
+            $"\nResult of Add: {MathOperation.Addition(a, b)}",
+            $"Result of Min: {MathOperation.Minus(a, b)}",
+            $"Result of Mult: {MathOperation.Multi(a, b)}",
+            $"Result of Div: {MathOperation.Devide(a, b)}",
+            $"Result of Max: {MathOperation.Max(a, b)}",
+            $"Minimum number of a and b: {MathOperation.Min(a, b)}",
+            $"Abs a: {absA}, Abs b: {absB}",
+            "\n\n\n\tThis app was created by TommyWindows."
+        };
+
+        foreach (string result in results)
+        {
+            input.InputString(result);
+            Console.WriteLine();
+        }
     }
 }
---
+++ Calculator/input.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace input
+{
+    internal class Input
+    {
+        public string InputString(string input) 
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                Thread.Sleep(50);
+                Console.Write(input[i]);
+            }
+            return input;
+        }
+    }
+}
